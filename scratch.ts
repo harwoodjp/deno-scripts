5ae493d48e5736ab14634879882c03050bf4e1fe
Justin Harwood <justin@harwoodjp.com>
2022-07-19 21:05:05 -0500

    fix scratch

--- scratch.ts
+++ scratch.ts
@@ -4,18 +4,19 @@ import {
   ensureFileSync,
 } from "https://deno.land/std/fs/mod.ts";
 
-const editor = "/Applications/Sublime\ Text.app/Contents/SharedSupport/bin/subl"
+const editor: string = "/Applications/Sublime\ Text.app/Contents/SharedSupport/bin/subl"
 const folder: string = "/Users/justin/Volumes/MEGA/Documents/scratch/"
 const extension: string = Deno.args[0] || "txt"
-const date: string = new Date().toISOString().substring(0, 10)
+const dateParts: string[] = new Date().toLocaleString("en-US", {timeZone: "America/Chicago"})
+	.split(",")[0]
+	.split("/")
+const date: string = `${dateParts[2]}-${dateParts[0]}-${dateParts[1]}`
 const file: string = `${folder}${date}.${extension}`
 
-exists(file)
+exists(file) 
 	.then(async (result : boolean) => {
-		if (result) {	
-		} else {
+		if (!result)
 			await ensureFile(file)
-		}
 	})
 	.then(async () => {
 			const cmd = [editor, file]
